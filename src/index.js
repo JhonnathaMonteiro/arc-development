382588c980e6d58b12d65642057ba33d4cabaf21
JhonnathaMonteiro <12281311+JhonnathaMonteiro@users.noreply.github.com>
2020-08-06 08:40:50 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+// Maybe withoyt StrictMode to avoid issues with material UI
 ReactDOM.render(
   <React.StrictMode>
     <App />
